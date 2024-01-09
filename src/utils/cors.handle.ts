ef5a8f2176e3acb938c0327c722c1789d1208cf8
EduBk <eduardo.mbks@gmail.com>
2024-01-09 14:28:09 -0600

    cors

--- src/utils/cors.handle.ts
+++ src/utils/cors.handle.ts
@@ -2,19 +2,18 @@
 import "dotenv/config";
 
 const base_url = process.env.BASE_URL as string;
-const whiteList = [base_url, "http://localhost:3001"];
+const whiteList = [base_url, "http://localhost:3000"];
 
 const corsOptions = {
-  origin: "*",
-  // origin: function (origin: any, callback: any) {
-  //   let corsPass = true;
-  //   if (whiteList.indexOf(origin) !== -1) {
-  //     corsPass = true;
-  //   } else {
-  //     corsPass = false;
-  //   }
-  //   callback(null, corsPass);
-  // },
+  origin: function (origin: any, callback: any) {
+    let corsPass = true;
+    if (whiteList.indexOf(origin) !== -1) {
+      corsPass = true;
+    } else {
+      corsPass = false;
+    }
+    callback(null, corsPass);
+  },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
