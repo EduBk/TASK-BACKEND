29ad7afa64dd870133f02482642d2d59b7606c74
Eduardo Martinez <enit.dev@gmail.com>
2024-09-20 13:04:06 -0600

    metodo ajustado

--- src/utils/jwt.handle.ts
+++ src/utils/jwt.handle.ts
@@ -1,23 +1,18 @@
-import { sign, verify } from 'jsonwebtoken';
+import { sign, verify } from "jsonwebtoken";
 const SESSION_SECRET = process.env.SESSION_SECRET as string;
 
 const generateToken = (payload: any) => {
-  console.log('Generating token with payload:', payload);
-  const jwt = sign(payload, SESSION_SECRET, {
-    expiresIn: '24h',
+  const data = JSON.parse(payload)
+  const jwt = sign(data, SESSION_SECRET, {
+    expiresIn: '24h'
   });
-  console.log('Generated token:', jwt);
+
   return jwt;
 };
 
 const verifyToken = (jwt: string) => {
-  try {
-    const decoded = verify(jwt, SESSION_SECRET);
-    return decoded;
-  } catch (error) {
-    console.error('Error verifying token:', error);
-    return null;
-  }
+  const isok = verify(jwt, SESSION_SECRET);
+  return isok;
 };
 
 export { generateToken, verifyToken };
